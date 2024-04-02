e7165c4e01f6546e1a86bf04d7181fec92bf8e44
davidcoats <david@getsafetycone.com>
2024-04-01 18:12:11 -0700

    Interim changes.

--- source/D8S.E0015/Code/Program.cs
+++ source/D8S.E0015/Code/Program.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Threading.Tasks;
 
 
 namespace D8S.E0015
 {
     class Program
     {
-        static async void Main()
+        static async Task Main()
         {
             await Explorations.Instance.Try_Generator_Host_cshtml();
         }
